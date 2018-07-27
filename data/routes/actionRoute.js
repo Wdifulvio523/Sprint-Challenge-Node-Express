7badf4c026cb832864a40d2cb6d34269e83855e9
Wdifulvio523 <w.difulvio523@gmail.com>
2018-07-27 11:29:10 -0400

    successfully rendered actions using router.get

---
+++ data/routes/actionRoute.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const actionModel = require('../helpers/actionModel');
+
+router.get('/', (req, res) => {
+    actionModel
+    .get()
+    .then(actions => {
+        res
+        .status(200)
+        .json(actions);
+    })
+    .catch(err => {
+        res
+        .status(500)
+        .json({ error: "The actions could not be retrieved."});
+});
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require('helmet');
 const morgan = require('morgan');
+const actionRoute = require('./data/routes/actionRoute');
 
 
 const server = express();
@@ -8,9 +9,7 @@ server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
 
-server.get("/", (req, res) => {
-    res.send("Hello World");
-  });
 
+server.use('/actions', actionRoute);
 
 server.listen(8000, () => console.log("Yo, Node Express API is running on port 8000"));
